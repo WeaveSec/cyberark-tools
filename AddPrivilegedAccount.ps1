988b80f5f7544830f2c7b9aff1a8b21c162756ba
WeaveSec <weavesec@gmail.com>
2023-10-31 16:25:59 +1100

    create safe via API

--- AddPrivilegedAccount.ps1
+++ AddPrivilegedAccount.ps1
@@ -4,7 +4,6 @@
 
 [System.Net.ServicePointManager]::ServerCertificateValidationCallback = {$true}
 
-
 # Define the URL of the API endpoint
 $url = "https://192.168.1.203/PasswordVault/API/auth/LDAP/Logon/"
 
@@ -14,37 +13,44 @@ $jsonBody = @{
     "password"="CrazyCats1!"
 } | ConvertTo-Json
 
-# Define the headers (if necessary)
-$headers = @{
-    "Content-Type"="application/json"
-}
-
 # Send the POST request
-$response = Invoke-RestMethod -Uri $url -Method Post -Body $jsonBody -Headers $headers
+$response = Invoke-RestMethod -Uri $url -Method Post -Body $jsonBody -ContentType "application/json"
 
 # Output the response
 Write-Output $response
 
-
+# Get All Safes
 #$get_all_safes_url = "https://192.168.1.203/PasswordVault/API/Safes/"
 #
 #$logonHeader =  New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
 #$logonHeader.Add("Authorization", $response)
 #
-#$get_all_safes_response = Invoke-RestMethod -Uri $get_all_safes_url -Headers $logonHeader -ContentType "application/json" -Method GET | Format-List
+#$get_all_safes_response = Invoke-RestMethod -Uri $get_all_safes_url -Headers $logonHeader -ContentType "application/json" -Method Get | Format-List
 #
 #
 #Write-Output $get_all_safes_response
 
-$get_all_accounts_url = "https://192.168.1.203/PasswordVault/API/Accounts/"
+# Get all Accounts
+#$get_all_accounts_url = "https://192.168.1.203/PasswordVault/API/Accounts/"
+#
+#$logonHeader =  New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+#$logonHeader.Add("Authorization", $response)
+#
+#$get_all_accounts_response = Invoke-RestMethod -Uri $get_all_accounts_url -Headers $logonHeader -ContentType "application/json" -Method GET | Format-List
+#
+#Write-Output $get_all_accounts_response
+
+# Create New Safe
+$create_new_safe_url = "https://192.168.1.203/PasswordVault/API/Safes/"
 
 $logonHeader =  New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
 $logonHeader.Add("Authorization", $response)
 
-$get_all_accounts_response = Invoke-RestMethod -Uri $get_all_accounts_url -Headers $logonHeader -ContentType "application/json" -Method GET | Format-List
+$json_new_safe_Body = @{
+  "managingCPM"= "passwordManager"
+  "safeName"= "TestSafe1"
+} | ConvertTo-Json
 
-Write-Output $get_all_accounts_response
+$create_new_safe_response = Invoke-RestMethod -Uri $create_new_safe_url  -Headers $logonHeader -Method Post -Body $json_new_safe_Body   -ContentType "application/json" | Format-List
 
-
-
-
+Write-Output $create_new_safe_response
